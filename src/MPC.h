362974f71da155b468c2db943cbdbd90d8a635fb
David Chrzanowski <chrzanowski.david@gmail.com>
2017-12-18 13:29:32 -0500

    add weights for cost function elements

--- src/MPC.h
+++ src/MPC.h
@@ -6,6 +6,15 @@
 
 using namespace std;
 
+// Set weights parameters for the cost function
+#define W_CTE 8.4
+#define W_EPSI 0.32
+#define W_V 0.261
+#define W_DELTA 600000
+#define W_A 17.1
+#define W_DDELTA 0.01
+#define W_DA 0.00001
+
 class MPC {
  public:
   MPC();
