ca7da27201ef01b972b39ca2448f9aa54c835f32
David Chrzanowski <chrzanowski.david@gmail.com>
2017-12-18 14:36:54 -0500

    correct double initialization of mpc_values

--- src/main.cpp
+++ src/main.cpp
@@ -162,13 +162,13 @@ int main() {
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
-          vector<double> mpc_x_vals; //= mpc.mpc_x;
-          vector<double> mpc_y_vals; //= mpc.mpc_y;
+          //vector<double> mpc_x_vals; //= mpc.mpc_x;
+          //vector<double> mpc_y_vals; //= mpc.mpc_y;
 
-          for(int i = 0; i<n; i++){
-          	mpc_x_vals.push_back(mpc.mpc_x[i]);
-          	mpc_y_vals.push_back(mpc.mpc_y[i]);
-          }
+          //for(int i = 0; i<n; i++){
+          	//mpc_x_vals.push_back(mpc.mpc_x[i]);
+          	//mpc_y_vals.push_back(mpc.mpc_y[i]);
+          //}
 
 
           msgJson["mpc_x"] = mpc_x_vals;
